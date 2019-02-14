d935bbf3a843bd3f69f5f1b206aa88933f084d8b
chaozding <dcsedu@foxmail.com>
2019-02-14 21:41:18 +0800

    add Block.go

---
+++ imooc.com/demochain/Block.go
@@ -0,0 +1,41 @@
+package demochain
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+)
+
+type Block struct {
+	Index         int64  //区块编号
+	Timestamp     int64  //区块时间戳，创建时间
+	PrevBlockHash string //
+	Hash          string //当前区块哈希值，不术语当前区块数据
+
+	Data string //区块数据
+}
+
+func calculateHash(b Block) string {
+	//当前区块哈希值不要包含
+	blockData := string(b.Index) + string(b.Timestamp) + string(b.PrevBlockHash) + string(b.Data)
+	hashInBytes := sha256.Sum256([]byte(blockData)) //字符串数据类型转换为字节切片
+	return hex.EncodeToString(hashInBytes[:])       //又转换为字符串了
+}
+
+func GenerateNewBlock(preBlock Block, data string) Block {
+	newBlock := Block{}
+	newBlock.Index = preBlock.Index + 1
+	newBlock.PrevBlockHash = preBlock.Hash
+	newBlock.Timestamp = time.Now().Unix()
+	newBlock.Hash = calculateHash(newBlock)
+
+	newBlock.Data = data
+	return newBlock
+}
+
+func GenerateGenesisBlock() Block {
+	preBlock := Block{}
+	preBlock.Index = -1
+	preBlock.Hash = ""
+	return GenerateNewBlock(preBlock, "Genesis Block")
+}
