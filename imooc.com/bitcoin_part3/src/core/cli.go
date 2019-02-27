6611b9309378d50ee40ca0c684fe400b0605a4a7
chaozding <dcsedu@foxmail.com>
2019-02-27 23:59:37 +0800

    add func (cli *CLI) validateArgs()

--- imooc.com/bitcoin_part3/src/core/cli.go
+++ imooc.com/bitcoin_part3/src/core/cli.go
@@ -1,6 +1,9 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 //CLI reponsible for processing command line arguments
 type CLI struct {
@@ -11,4 +14,11 @@ func (cli *CLI) printUsage() {
 	fmt.Println("Usage:")
 	fmt.Println("  addblock -data BLOCK_DATA - add a block to the blockchain")
 	fmt.Println("  printchain - print all the blocks of the blockchain ")
+}
+
+func (cli *CLI) validateArgs() { //验证参数
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
 }
