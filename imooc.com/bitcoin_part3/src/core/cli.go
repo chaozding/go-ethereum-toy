1383575d21e1c8c9dcd57f753877829835e0e155
chaozding <dcsedu@foxmail.com>
2019-02-27 23:46:53 +0800

    add func (cli *CLI) printUsage()

--- imooc.com/bitcoin_part3/src/core/cli.go
+++ imooc.com/bitcoin_part3/src/core/cli.go
@@ -1,6 +1,14 @@
 package core
 
+import "fmt"
+
 //CLI reponsible for processing command line arguments
 type CLI struct {
 	Bc *Blockchain
+}
+
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  addblock -data BLOCK_DATA - add a block to the blockchain")
+	fmt.Println("  printchain - print all the blocks of the blockchain ")
 }
