4939e41bc6a4d732d61f00108ccff7095a1903e0
chaozding <dcsedu@foxmail.com>
2019-03-17 13:21:58 +0800

    block.go

--- imooc.com/bitcoin_part4/src/core/block.go
+++ imooc.com/bitcoin_part4/src/core/block.go
@@ -18,15 +18,15 @@ type Block struct {
 	PreBlockHash []byte         //前一个区块的哈希值，也可以是string，不过[]byte更简单
 	Hash         []byte         //区块自身的哈希值，用于校验区块的数据有效性
 
-	Nonce int //工作量证明
+	Nonce int //工作量
 }
 
-//Serialize serializes the block 强行编码，序列化区块
+//Serialize serializes the block 强行编码，序列化区块，把区块结构体序列化为[]byte字符数组类型， 便于存储
 func (b *Block) Serialize() []byte {
-	var result bytes.Buffer //临时缓存区域
-	encoder := gob.NewEncoder(&result)
+	var result bytes.Buffer            //临时缓存区域
+	encoder := gob.NewEncoder(&result) //传入缓存区，便于存放编码后的结构体对象Block
 
-	err := encoder.Encode(b)
+	err := encoder.Encode(b) //编码
 	if err != nil {
 		log.Panic(err)
 	}
@@ -34,24 +34,41 @@ func (b *Block) Serialize() []byte {
 	return result.Bytes() //通过自身类库转成了字节数组类型
 }
 
-func NewGenesisBlock(coinbase *Transaction) *Block { //返回的是一个区块结构结构体的指针
-	//return NewBlock("Genesis Block", []byte{}) //空数据
-	return NewBlock([]*Transaction{coinbase}, []byte{}) //只有一个发币交易,用发币交易初始化数组
+//DeserializeBlock deserializes a block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d)) //准备好解码了，因为d是编码后的，所以需要读取
+	err := decoder.Decode(&block)                 //执行解码并存储到结构体对象里面
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
 
 //经过了工作证明的区块
 func NewBlock(transactions []*Transaction, preBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), transactions, preBlockHash, []byte{}, 0}
+	block := &Block{
+		time.Now().Unix(),
+		transactions,
+		preBlockHash,
+		[]byte{},
+		0}
 	//block.SetHash()
 	pow := NewProofOfWork(block) //可以理解为类，然后就不用传递数据了
-	//可以理解为类的构造函数
-	nonce, hash := pow.Run() //为什么不用pow.Run(block)
-	//就是挖矿了
+	//可以理解为类的构造函数，用类的方法进行工作量证明操作
+	nonce, hash := pow.Run() //就是挖矿了
 
 	block.Hash = hash[:] //满足系统规定工作量条件的哈希值
 	block.Nonce = nonce  //可以理解为工作量完成条件
 
 	return block
+}
+
+func NewGenesisBlock(coinbase *Transaction) *Block { //返回的是一个区块结构结构体的指针
+	//return NewBlock("Genesis Block", []byte{}) //空数据
+	return NewBlock([]*Transaction{coinbase}, []byte{}) //只有一个发币交易,用发币交易初始化数组
 }
 
 //func (b *Block) SetHash() {
@@ -60,20 +77,6 @@ func NewBlock(transactions []*Transaction, preBlockHash []byte) *Block {
 //	hash := sha256.Sum256(headers) //headers其实包含了数据部分，hash类型是字节数组[]byte
 //	b.Hash = hash[:]               //转换为字节数组
 //}
-
-//DeserializeBlock deserializes a block
-//为什么要从字节数组解码区块
-func DeserializeBlock(d []byte) *Block {
-	var block Block
-
-	decoder := gob.NewDecoder(bytes.NewReader(d)) //准备好解码了
-	err := decoder.Decode(&block)                 //执行解码
-	if err != nil {
-		log.Panic(err)
-	}
-
-	return &block
-}
 
 //计算区块里所有交易的哈希
 func (b *Block) HashTransactions() []byte {
