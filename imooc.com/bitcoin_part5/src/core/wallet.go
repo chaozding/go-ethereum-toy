cae393cc78596cfcc7a77b842979a8f32854d9f1
chaozding <dcsedu@foxmail.com>
2019-03-24 17:03:40 +0800

    add func NewWallet() *Wallet

--- imooc.com/bitcoin_part5/src/core/wallet.go
+++ imooc.com/bitcoin_part5/src/core/wallet.go
@@ -1,1 +1,21 @@
 package core
+
+import "crypto/ecdsa"
+
+const versiion = byte(0x00)
+const walletFile = "wallet.dat"
+const addressChecksumLen = 4
+
+//Wallet stores private and public keys
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+//NewWallet creates and returns a Wallet
+func NewWallet() *Wallet {
+	private, public := newKeyPair()
+	wallet := Wallet{private, public}
+
+	return &wallet
+}
