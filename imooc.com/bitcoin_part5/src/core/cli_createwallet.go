e5685c903ba04105268bdbefb4532f3429955882
chaozding <dcsedu@foxmail.com>
2019-03-24 16:54:54 +0800

    add func NewWallets() (*Wallets, error)

--- imooc.com/bitcoin_part5/src/core/cli_createwallet.go
+++ imooc.com/bitcoin_part5/src/core/cli_createwallet.go
@@ -3,7 +3,7 @@ package core
 import "fmt"
 
 func (cli *CLI) createWallet() {
-	wallets, _ := NewWallets()        //创建了一个钱包集合对象
+	wallets, _ := NewWallets()        //创建了一个钱包集合对象， 如果钱包集合已经存在则不需要再重复创建了
 	address := wallets.CreateWallet() //在钱包集合里面创建一个钱包对象
 	wallets.SaveToFile()              //把钱包对象存储到钱包集合文件
 
--- imooc.com/bitcoin_part5/src/core/wallets.go
+++ imooc.com/bitcoin_part5/src/core/wallets.go
@@ -1,1 +1,16 @@
 package core
+
+//Wallets stores a collection of wallets
+type Wallets struct {
+	Wallets map[string]*Wallets
+}
+
+//NewWallets creates Wallets and fills it from a file if it exists
+func NewWallets() (*Wallets, error) {
+	wallets := Wallets{}
+	wallets.Wallets = make(map[string]*Wallets)
+
+	err := wallets.LoadFromFile() //从已有的文件中读取，初始化包含在创建钱包里面
+
+	return &wallets, err //错误返回有什么用
+}
