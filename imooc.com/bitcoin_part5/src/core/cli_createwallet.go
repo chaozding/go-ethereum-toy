714baf3953edc2aa99ea39ea7f54a730724eea30
chaozding <dcsedu@foxmail.com>
2019-03-24 10:05:15 +0800

    add func (cli *CLI) createWallet()

--- imooc.com/bitcoin_part5/src/core/cli_createwallet.go
+++ imooc.com/bitcoin_part5/src/core/cli_createwallet.go
@@ -1,1 +1,11 @@
 package core
+
+import "fmt"
+
+func (cli *CLI) createWallet() {
+	wallets, _ := NewWallets()        //创建了一个钱包集合对象
+	address := wallets.CreateWallet() //在钱包集合里面创建一个钱包对象
+	wallets.SaveToFile()              //把钱包对象存储到钱包集合文件
+
+	fmt.Printf("Your new address: %s\n", address)
+}
